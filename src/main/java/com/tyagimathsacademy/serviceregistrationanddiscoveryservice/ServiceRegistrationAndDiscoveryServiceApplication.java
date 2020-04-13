86bffa3882724ed6181cadc5d7b080ca666b7585
Ankit Tyagi <ankit10.iitd@gmail.com>
2020-04-14 01:41:36 +0530

    Initial service registery commit

---
+++ src/main/java/com/tyagimathsacademy/serviceregistrationanddiscoveryservice/ServiceRegistrationAndDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.tyagimathsacademy.serviceregistrationanddiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistrationAndDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationAndDiscoveryServiceApplication.class, args);
+	}
+
+}
